b6e6908bff73899b4f3a04c86305206e3702c705
mlodovico <murilo.lodovico@outlook.com>
2023-08-31 00:13:38 -0300

    feat: pill component

---
+++ src/components/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <PillComponent v-for="item in groupedWords" :word="item.word" :amount="item.amount" :key="item.word"/>
+  </div>
+</template>
+
+<script>
+import PillComponent from './PillComponent';
+export default {
+    PillComponent,
+    data: function() {
+        return {
+            groupedWords: [
+            {word:"i", amount: 450},
+            {word:"am", amount: 1150},
+            {word:"can", amount: 700}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/PillComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="pill">
+    <span class="word">{{ word }}</span>
+    <span class="amount">{{ amount }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ["word", "amount"]
+}
+</script>
+
+<style scoped>
+    .pill {
+        margin: 10px;
+        height: 55px;
+        border-radius: 35px;
+        color: #fff;
+        font-size: 1.3rem;
+        font-weight: bold;
+        display: flex;
+        align-items: center;
+        background: red;
+    }
+
+    .pill .word {
+        margin-left: 15px;
+
+    } 
+
+    .pill .amount {
+        width: 40px;
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 10px;
+        padding: 5px;
+        border-radius: 50%;
+        background: #fff;
+        color: red;
+        font-size: 14px;
+    }
+</style>
