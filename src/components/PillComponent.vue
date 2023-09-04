4431d0ee7f1db9e944056eba64277ab32ff17e5c
mlodovico <murilo.lodovico@outlook.com>
2023-09-04 14:34:17 -0300

    feat: interface colors

--- src/components/PillComponent.vue
+++ src/components/PillComponent.vue
@@ -1,13 +1,32 @@
 <template>
-  <div class="pill">
-    <span class="word">{{ word }}</span>
-    <span class="amount">{{ amount }}</span>
+      <div class="pill" :style="{ backgroundColor: amountColor }">
+        <span class="word">{{ word }}</span>
+        <span class="amount" :style="{ backgroundColor: amountColor }">{{ amount }}</span>
   </div>
 </template>
 
 <script>
 export default {
-    props: ["word", "amount"]
+    props: ["word", "amount"],
+    computed: {
+        amountColor() {
+            if (this.amount > 1000) {
+                return "#ff061a"
+            } else if (this.amount > 700) {
+                return "#ff6221"
+            } else if (this.amount > 500) {
+                return "#ff9e31"
+            } else if (this.amount > 200) {
+                return "#f2d040"
+            } else if (this.amount > 100) {
+                return "#00d13c"
+            } else if (this.amount > 50) {
+                return "#0073c3"
+            } else {
+                return "#9175a1"
+            }
+        }
+    }
 }
 </script>
 
@@ -22,7 +41,6 @@ export default {
         font-weight: bold;
         display: flex;
         align-items: center;
-        background: red;
     }
 
     .pill .word {
@@ -40,7 +58,6 @@ export default {
         padding: 5px;
         border-radius: 50%;
         background: #fff;
-        color: red;
         font-size: 14px;
     }
 </style>
